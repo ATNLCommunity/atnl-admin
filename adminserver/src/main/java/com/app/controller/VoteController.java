a49410fb634358fecd16e6dc6786b6914c5b78ee
xjon <xjon@qq.com>
2018-06-05 23:51:48 +0800

    vote seed

--- adminserver/src/main/java/com/app/controller/VoteController.java
+++ adminserver/src/main/java/com/app/controller/VoteController.java
@@ -2,8 +2,11 @@ package com.app.controller;
 
 import java.util.List;
 
+import com.app.model.VoteSeed;
 import com.jfinal.plugin.activerecord.Db;
 import com.jfinal.plugin.activerecord.Record;
+
+import org.apache.commons.lang3.StringUtils;
 
 import n.fw.base.BaseController;
 
@@ -19,5 +22,89 @@ public class VoteController extends BaseController
 
         List<Record> arr = Db.query("SELECT idx,COUNT(1) FROM vote GROUP BY idx");
         success(arr);
+    }
+
+    public void seed()
+    {
+        if (!checkLevel(1))
+        {
+            errorInvalidOper();
+            return;
+        }
+
+        List<VoteSeed> arr = VoteSeed.dao.getAll();
+        success(arr);
+    }
+
+    public void add()
+    {
+        if (!checkLevel(2))
+        {
+            errorInvalidOper();
+            return;
+        }
+
+        String name = getPara("name", "");
+        String logo = getPara("logo", "");
+        float rate = Float.valueOf(getPara("rate", "0"));
+        if (StringUtils.isBlank(name) || StringUtils.isBlank(logo))
+        {
+            errorInvalid();
+            return;
+        }
+
+        VoteSeed seed = new VoteSeed();
+        seed.set(VoteSeed.NAME, name);
+        seed.set(VoteSeed.LOGO, logo);
+        seed.set(VoteSeed.RATE, rate);
+
+        seed.save();
+        success(seed);
+    }
+
+    public void edit()
+    {
+        if (!checkLevel(2))
+        {
+            errorInvalidOper();
+            return;
+        }
+
+        Long id = getParaToLong("id", 0L);
+        String name = getPara("name", "");
+        String logo = getPara("logo", "");
+        float rate = Float.valueOf(getPara("rate", "0"));
+        if (StringUtils.isBlank(name) || StringUtils.isBlank(logo))
+        {
+            errorInvalid();
+            return;
+        }
+
+        VoteSeed seed = VoteSeed.dao.findBySid(id);
+        if (seed == null)
+        {
+            error("找不到改选项");
+            return;
+        }
+
+        seed.set(VoteSeed.NAME, name);
+        seed.set(VoteSeed.LOGO, logo);
+        seed.set(VoteSeed.RATE, rate);
+        seed.update();
+
+        success(seed);
+    }
+
+    public void del()
+    {
+        if (!checkLevel(2))
+        {
+            errorInvalidOper();
+            return;
+        }
+
+        Long id = getParaToLong("id", 0L);
+        Db.update("DELETE FROM voteseed WHERE id=?", id);
+        success("成功");
     }
 }
---
+++ adminserver/src/main/java/com/app/model/VoteSeed.java
@@ -0,0 +1,27 @@
+package com.app.model;
+
+import java.util.List;
+
+import com.jfinal.plugin.activerecord.Model;
+
+public class VoteSeed extends Model<VoteSeed>
+{
+    private static final long serialVersionUID = 3554755307155224125L;
+
+    public static final VoteSeed dao = new VoteSeed();
+
+    public static final String ID = "id";
+    public static final String NAME = "name";
+    public static final String RATE = "rate";
+    public static final String LOGO = "logo";
+
+    public List<VoteSeed> getAll()
+    {
+        return find("SELECT * FROM voteseed");
+    }
+
+    public VoteSeed findBySid(Long id)
+    {
+        return findFirst("SELECT * FROM voteseed WHERE id=?", id);
+    }
+}
