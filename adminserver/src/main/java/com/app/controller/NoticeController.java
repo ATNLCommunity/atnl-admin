e5ea118959413b9fbc2aac56e24011d756c99b89
zhangwen <364390162@qq.com>
2018-07-07 13:21:58 +0800

    new file:   adminserver/src/main/java/com/app/controller/NoticeController.java 	new file:   adminserver/src/main/java/com/app/model/Notice.java

---
+++ adminserver/src/main/java/com/app/controller/NoticeController.java
@@ -0,0 +1,127 @@
+package com.app.controller;
+
+import com.app.model.Notice;
+
+
+import n.fw.base.BaseController;
+
+public class NoticeController extends BaseController
+{
+    /**
+     * 分页获取公告信息
+     */
+	public void getNotice()
+	{
+		int page = getParaToInt("page",0);
+		if(page <= 0)
+		{
+			page = 0;
+		}
+		else
+		{
+			page = page -1;
+		}
+		success(Notice.dao.list(page));
+	}
+	
+	/**
+	 * 增加公告信息
+	 */
+	public void addNotice()
+	{
+		if (!checkLevel(1))
+        {
+            errorInvalidOper();
+            return;
+        }
+		Long aid = getUid();
+		if(aid == 0)
+		{
+			error("请重新登陆");
+			return;
+		}
+		String title = getPara("title","");
+		String content = getPara("content","");
+		if(title.trim().length() == 0 || content.trim().length() == 0)
+		{
+			error("标题和内容都不能为空");
+			return;
+		}
+		success(Notice.dao.create(aid, title, content));	
+	}
+	/**
+	 * 修改公告信息
+	 */
+	
+	public void updateNotice()
+	{
+		if (!checkLevel(1))
+        {
+            errorInvalidOper();
+            return;
+        }
+		Long aid = getUid();
+		if(aid == 0)
+		{
+			error("请重新登陆");
+			return;
+		}
+		Long id = getParaToLong("id",0L);
+		if(id == 0L)
+		{
+			error("不存在的公告id");
+			return;
+		}
+		Notice notice = Notice.dao.findById(id);
+		if(null == notice)
+		{
+			error("不存在的公告id");
+			return;
+		}
+		String title = getPara("title","");
+		String content = getPara("content","");
+		if(title.trim().length() == 0 || content.trim().length() == 0)
+		{
+			error("公告标题和公告内容不能为空");
+			return;
+		}
+		notice.set(Notice.AID, aid);
+		notice.set(Notice.TIILE, title);
+		notice.set(Notice.CONTENT, content);
+		notice.update();
+		
+		success(notice);
+		
+	}
+	
+	/*
+	 * 删除公告
+	 */
+	public void deleteById()
+	{
+		if (!checkLevel(1))
+        {
+            errorInvalidOper();
+            return;
+        }
+		Long aid = getUid();
+		if(aid == 0)
+		{
+			error("请重新登陆");
+			return;
+		}
+		Long id = getParaToLong("id",0L);
+		if(id == 0L)
+		{
+			error("不存在的公告id");
+			return;
+		}
+		Notice notice = Notice.dao.findById(id);
+		if(null == notice)
+		{
+			error("不存在的公告id");
+			return;
+		}
+		success(notice.delete());
+	}
+}
---
+++ adminserver/src/main/java/com/app/model/Notice.java
@@ -0,0 +1,42 @@
+package com.app.model;
+
+
+import java.util.List;
+
+import com.jfinal.plugin.activerecord.Model;
+
+import n.fw.utils.DateUtils;
+
+// 后台公告表
+public class Notice extends Model<Notice>
+{
+    private static final long serialVersionUID = 3554755307155224108L;
+
+    public static final Notice dao = new Notice();
+
+    public static final String ID = "id";       // ID
+    public static final String AID = "aid"; // 发公告的管理员id
+    public static final String SENDTIME = "sendtime";   // 发送时间
+    public static final String TIILE = "title";     // 标题
+    public static final String CONTENT = "content"; //内容
+
+    public Notice create(Long aid,String title,String content)
+    {
+        Notice notice = new Notice();
+        notice.set(Notice.AID, aid);
+        notice.set(Notice.TIILE, title);
+        notice.set(Notice.CONTENT, content);
+        notice.set(Notice.SENDTIME, DateUtils.getDateTime());
+        
+        if(notice.save())
+        {
+        	return notice;
+        }
+        return null;
+    }
+    
+    public List<Notice> list(int page)
+    {
+    	return find("select * from notice limit ?,20",page*20);
+    }
+}
